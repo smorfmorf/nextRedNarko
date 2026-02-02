40b3ce41707f9c039bb1f91a69d3fdc8021b5693
M.P. Yuzik <m.p.yuzik@example.com>
2026-02-02 16:42:45 +1000

    ''

---
+++ prisma/seed.ts
@@ -0,0 +1,150 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { hashSync } from "bcrypt";
+
+const prisma = new PrismaClient();
+
+// cascade - значит если есть какие-то взаимосвязанные данные, то они будут удалены (из других таблиц)
+async function deleteAll() {
+  //   await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+  // 1️⃣ ВЫКЛЮЧАЕМ foreign keys
+  await prisma.$executeRaw`PRAGMA foreign_keys = OFF;`;
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "User"`]);
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "Category"`]);
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "Ingredient"`]);
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "Product"`]);
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "ProductItem"`]);
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "Cart"`]);
+  await prisma.$transaction([prisma.$executeRaw`DELETE FROM "CartItem"`]);
+
+  // 3️⃣ ВКЛЮЧАЕМ обратно
+  await prisma.$executeRaw`PRAGMA foreign_keys = ON;`;
+}
+
+async function download() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User",
+        email: "user@ya.ru",
+        password: hashSync("666", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+    ],
+  });
+
+  //   CATEGORIES
+  const categoris = [
+    {
+      name: "Драги",
+    },
+    {
+      name: "КОКА-КОЛЫ",
+    },
+    {
+      name: "КОКИ",
+    },
+  ];
+  await prisma.category.createMany({
+    data: categoris,
+  });
+
+  // INGREDIENTS --------------------------------------
+  const ingredients = [
+    {
+      name: "размешаная соляга",
+      price: 100,
+      imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf3e424371b49f0b8d7dbe320a70.avif",
+    },
+    {
+      name: "нфтзи",
+      price: 200,
+      imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf3e424371b49f0b8d7dbe320a70.avif",
+    },
+  ].map((o, i) => ({ ...o, id: i + 1 }));
+  await prisma.ingredient.createMany({
+    data: ingredients,
+  });
+  //   PRODUCTS --------------------------------------
+  const products = [
+    {
+      name: "Лате",
+      imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf3e424371b49f0b8d7dbe320a70.avif",
+      categoryId: 1,
+    },
+  ];
+
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  const nark1 = await prisma.product.create({
+    data: {
+      name: "Narko1",
+      imageUrl: "A2.webp",
+      categoryId: 3,
+      ingredients: {
+        connect: ingredients.slice(0, 1),
+      },
+    },
+  });
+
+  //   PRODUCTS ITEM --------------------------------------
+  interface PropsProductItem {
+    productId: number;
+    pizzaType?: 1 | 2;
+    size?: 20 | 30 | 40;
+  }
+  function generateProductItem({
+    productId,
+    pizzaType,
+    size,
+  }: PropsProductItem): Prisma.ProductItemUncheckedCreateInput {
+    return {
+      productId,
+      pizzaType,
+      size,
+      price: Math.floor(Math.random() * 1000),
+    };
+  }
+
+  await prisma.productItem.createMany({
+    data: [
+      generateProductItem({ productId: nark1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: nark1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: nark1.id, pizzaType: 1, size: 20 }),
+    ],
+  });
+
+  await prisma.cart.createMany({
+    data: [
+      {
+        userId: 1,
+        totalAmount: 0,
+        userToken: "wddwadawdawd12",
+      },
+    ],
+  });
+
+  await prisma.cartItem.create({
+    data: {
+      cartId: 1,
+      productItemId: 1,
+      quantity: 2,
+      ingredients: {
+        connect: ingredients.slice(0, 1),
+      },
+    },
+  });
+}
+
+async function main() {
+  try {
+    await deleteAll();
+    await download();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main().then(() => prisma.$disconnect());
