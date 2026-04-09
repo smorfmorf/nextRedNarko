7dbf6a7d6096b6a034eb6a9dd708de8ac83220b0
Максим Ю <malalamobilelegends@gmail.com>
2026-04-09 14:58:13 +0000

    ''

---
+++ app/types.d.ts
@@ -0,0 +1,2 @@
+declare module '*.css';
+declare module 'react-dadata/dist/react-dadata.css';
--- components/form/addres-input.tsx
+++ components/form/addres-input.tsx
@@ -5,11 +5,14 @@ import { AddressSuggestions } from "react-dadata";
 import "react-dadata/dist/react-dadata.css";
 
 interface Props {
-  onChange?: (value?: string) => void;
+    onChange?: (value?: string) => void;
 }
 
 export const AdressInput: React.FC<Props> = ({ onChange }) => {
-  return (
-    <AddressSuggestions token="b5b8bb983ddcd08648080e0271d9dd367bb7aa65" onChange={(data) => onChange?.(data?.value)} />
-  );
+    return (
+        <AddressSuggestions
+            token="ee7004820e7c25e87bf7ae0a5aa936c5292a49fd"
+            onChange={(data) => onChange?.(data?.value)}
+        />
+    );
 };
